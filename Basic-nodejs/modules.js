0075d62355eced28bc5acc5245bf332f8b43c964
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-03-13 16:15:35 +0100

    modules and file imports

---
+++ Basic-nodejs/modules.js
@@ -0,0 +1,11 @@
+const { people, ages } = require('./people');
+
+// Imports people file from the same directory and then it also takes out the specified parameters from the imported file
+
+// Importing a file doesnt give automatic access to the file
+
+console.log(people, ages);
+
+const os = require('os');
+
+console.log(os.platform(), os.homedir());
---
+++ Basic-nodejs/people.js
@@ -0,0 +1,10 @@
+const people = ['yoshi', 'rup', 'Nche', 'Wardu'];
+
+const ages = [23,24, 34, 78];
+
+
+module.exports = {
+    people, ages
+}
+// The above code has to be entered to represent what is being exported.
+// For the sake of specificity.
