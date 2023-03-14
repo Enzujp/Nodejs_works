1bc472f13794933ed2984006d407952f9cd00435
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-03-14 15:26:39 +0100

    practicing modules and imports

--- Basic-nodejs/modules.js
+++ Basic-nodejs/modules.js
@@ -1,11 +1,15 @@
-const { people, ages } = require('./people');
+// const { people, ages } = require('./people');
 
-// Imports people file from the same directory and then it also takes out the specified parameters from the imported file
+// // Imports people file from the same directory and then it also takes out the specified parameters from the imported file
 
-// Importing a file doesnt give automatic access to the file
+// // Importing a file doesnt give automatic access to the file
+
+// console.log(people, ages);
+
+// const os = require('os');
+
+// console.log(os.platform(), os.homedir());
+
+const {people, ages} = require('./people');
 
 console.log(people, ages);
-
-const os = require('os');
-
-console.log(os.platform(), os.homedir());
--- Basic-nodejs/people.js
+++ Basic-nodejs/people.js
@@ -1,10 +1,18 @@
-const people = ['yoshi', 'rup', 'Nche', 'Wardu'];
+// const people = ['yoshi', 'rup', 'Nche', 'Wardu'];
 
-const ages = [23,24, 34, 78];
+// const ages = [23,24, 34, 78];
 
+
+// module.exports = {
+//     people, ages
+// }
+// The above code has to be entered to represent what is being exported.
+// For the sake of specificity.
+
+const people = ['yoshi',  'yuppy', 'palo', 'alto']
+
+const ages = [23, 32, 54, 63]
 
 module.exports = {
     people, ages
 }
-// The above code has to be entered to represent what is being exported.
-// For the sake of specificity.
