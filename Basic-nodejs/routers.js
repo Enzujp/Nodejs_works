fedb4db6984104cc5c6aecc728bbb53954b7fa37
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-03-16 13:30:28 +0100

    here's how you create a server an write a function so the server listens

--- Basic-nodejs/routers.js
+++ Basic-nodejs/routers.js
@@ -1,1 +1,12 @@
-// learning a little about IP addresses
+const http = require('http');
+// the createServer function takes in a callback argument
+const server = http.createServer((req, res) => {
+    console.log('request made');
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('listening for requests on port 3000')
+});
+// server.listen listens for requests, takes in 3 arguments
+// port number, localhost on default and a function 
+
