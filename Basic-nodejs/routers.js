120f36ee7b6fbeb84bcb89aa29b1c68d987edfab
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-03-16 15:27:04 +0100

    added status codes

--- Basic-nodejs/routers.js
+++ Basic-nodejs/routers.js
@@ -14,12 +14,15 @@ const server = http.createServer((req, res) => {
     switch(req.url) {
         case '/':
             path += 'index.html'
+            res.statusCode = 200;
             break;
         case '/about':
             path += 'about.html';
+            res.statusCode = 200;
             break;
         default:
             path += '404.html';
+            res.statusCode = 404;
             break;
     }
 
@@ -32,6 +35,8 @@ const server = http.createServer((req, res) => {
             // res.write(data);
             // if we're sending one thing to the server we could make use 
             // of the res.end() and parse in the data like res.end(data)
+            
+            
             res.end(data);
             // however for multiple data entries we might need to be more specific
         }
@@ -48,3 +53,5 @@ server.listen(3000, 'localhost', () => {
 // port number, localhost on default and a function 
 
 // you have to restart your server when you make changes
+
+
