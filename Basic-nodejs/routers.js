d13f7280b122e459680fe2db143f9ed0b9cd9488
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-03-16 14:05:03 +0100

    added more notes to this p

--- Basic-nodejs/routers.js
+++ Basic-nodejs/routers.js
@@ -1,7 +1,18 @@
 const http = require('http');
 // the createServer function takes in a callback argument
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    console.log(req.url, req.method);
+
+    // set header content type
+    res.setHeader('content-Type', 'text/html');
+
+    res.write('<head><link rel="stylesheet" href="#"></head>')
+    res.write('<p>hello, enzu<p>');
+    res.write('<p>hello, baby boy<p>');
+    res.end();
+
+
+    // ideally html pages would be included in a different sheet and then imported in here.
 });
 
 server.listen(3000, 'localhost', () => {
@@ -10,3 +21,4 @@ server.listen(3000, 'localhost', () => {
 // server.listen listens for requests, takes in 3 arguments
 // port number, localhost on default and a function 
 
+// you have to restart your server when you make changes
